b673e0c69605f03a9f4509eeddb64fae020547cb
yunfeng.chen <yunfeng.chen@lkcoffee.com>
2024-10-20 23:47:30 +0800

    12.3&12.4 重构minigrep & TDD 实现search

---
+++ minigrep/src/main.rs
@@ -0,0 +1,38 @@
+// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+/**
+保留在main函数中的职责应限于以下内容：
+Calling the command line parsing logic with the argument values
+使用参数值调用命令行解析逻辑
+Setting up any other configuration
+设置任何其他配置
+Calling a run function in lib.rs
+调用lib.rs中的run函数
+Handling the error if run returns an error
+如果run返回错误，则处理错误
+ */
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config.clone()) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+    // 如果想多次使用config调用run，就需要对Config实现Clone，并在前面的调用中使用config.clone()
+    // if let Ok(res) = minigrep::run(config) {
+    //     println!("Run was successful, result: {:?}", res);
+    //     process::exit(1);
+    // }
+}
